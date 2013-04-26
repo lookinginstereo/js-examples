129c9b4b0ccc046c822ba70226653191a5ef5a21
Willie <williem@lexmark.com>
2013-04-26 08:43:18 -0400

    Adding comments/explanation to Comment model

--- backbone-example/models/comment.js
+++ backbone-example/models/comment.js
@@ -1,4 +1,10 @@
+//Here is the Comment Model -- its pretty simple. Almost all of
+//the functionality is BUILTIN to backbone for dealing with the models.
 var Comment = Backbone.Model.extend({
+    //The only thing we're using here is the defaults parameter which
+    //sets the default values for attributes if they're NOT set with 
+    //the constructor. This isn't needed, but it is helpful for
+    //knowing what the Model's properties should be
     defaults : {
         name : "No Name",
         comment : "No Comment",
